9f148f83333afffd915c23895ddbbc7eed7561e9
YastrebCode <gueststrue1@gmail.com>
2024-10-14 20:06:27 +0300

    v2.0

---
+++ atr_met_of_objects_new.py
@@ -0,0 +1,99 @@
+
+class House:
+    houses_history = []
+    #__instance = None
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        #if cls.__instance is None:
+            #cls.__instance = super().__new__(cls)
+        #return cls.__instance
+        return super().__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if new_floor > self.number_of_floors or new_floor < 1:
+            print('Такого этажа не существует')
+        else:
+            for i in range(1, new_floor):
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        if self.number_of_floors == other.number_of_floors:
+            return True
+        else:
+            return False
+
+    def __lt__(self, other):
+        if self.number_of_floors < other.number_of_floors:
+            return True
+        else:
+            return False
+
+    def __le__(self, other):
+        if self.number_of_floors <= other.number_of_floors:
+            return True
+        else:
+            return False
+
+    def __gt__(self, other):
+        if self.number_of_floors > other.number_of_floors:
+            return True
+        else:
+            return False
+
+    def __ge__(self, other):
+        if self.number_of_floors >= other.number_of_floors:
+            return True
+        else:
+            return False
+
+    def __ne__(self, other):
+        if self.number_of_floors != other.number_of_floors:
+            return True
+        else:
+            return False
+
+    def __add__(self, value):
+        self.number_of_floors = self.number_of_floors + value
+        return self
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __radd__(self, value):
+        self.number_of_floors = value + self.number_of_floors
+        return self
+
+    def __del__(self):
+        print(f'{self.name} снесён, но он остается в истории')
+
+
+
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
+
+
+
+
+
